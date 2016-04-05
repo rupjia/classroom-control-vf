f071771ee0c07798f02f366d7e1390007fd018a4
Rupji, Amey <RupjiA@advisory.com>
2016-04-05 09:15:17 -0500

    EDITED sites.pp to manage /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,12 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 }
+
+# Excercise 7.1
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Think before you type\n",
+}
