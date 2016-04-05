a11906035dbe57aaa8a3916a5b69359124d2c354
Rupji, Amey <RupjiA@advisory.com>
2016-04-05 14:54:36 -0500

    Remove extra , from manifest/init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -6,7 +6,7 @@ class skeleton {
     mode    => '0755',
   }
   file { '/etc/skel':
-    ensure  => file,,
+    ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
