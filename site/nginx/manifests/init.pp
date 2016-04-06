eb9350344228611825d17dd8ad448a15186494fa
Rupji, Amey <RupjiA@advisory.com>
2016-04-06 13:42:19 -0500

    Ex 13.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,38 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
-  }
-  file { '/var/www':
-    ensure => directory,
+  $nginx = 'nginx'
+  $root = '/var/www'
+  $index_src = "puppet:///modules/${nginx}/index.html"
+  $nginx_conf_src = "puppet:///modules/${nginx}/nginx.conf"
+  $nginx_default_scr = "puppet:///modules/${nginx}/default.conf"
+
+  File {
     owner => 'root',
     group => 'root',
-    mode => '0775',
+    mode => '0664',
+  }
+  package { $nginx:
+    ensure => present,
+  }
+  file { [ $root, '/etc/nginx/conf.d' ]:
+    ensure => directory,
   }
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/index.html',
+    source => $index_src,
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/nginx.conf',
+    source => $nginx_conf_src,
     require => Package['nginx'],
     notify => Service['nginx'],
-  }
-  file { '/etc/nginx/conf.d':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+    source => $nginx_default_scr,
     notify => Service['nginx'],
+    require => Package['nginx'],
   }
-  service { 'nginx':
+  service { $nginx:
     ensure => running,
     enable => true,
   }
